648bfe6837669e93800b61f6a2eeda474205a3c8
Pasha-nineseven <pavel@nineseven.ru>
2019-03-23 07:42:49 +0300

    init

---
+++ app/js/interface.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	flexibility(document.documentElement);
+	// $("body").on("click", ".test", function(e){
+	// 	e.preventDefault();
+	// })
+
+});
+
+
+
+
+$(window).resize(function () {
+
+});
+
+// $(window).load(function(){
+
+// });
+
+// functions
+
+
+// links pages
+// $('body').append(
+// 	'<div style="position: fixed; z-index: 1005; bottom: 0; right: 0; background: #fff; border: solid 1px #828286; width: 200px;"> \
+// 		<a href="javascript:void(0);" style="float: right;background:#ccc; color:#000; padding: 5px 10px; text-decoration: none; font-size: 16px" onclick="$(this).parent().hide()">Close X</a> \
+// 	<style> \
+// 		#pages { padding: 10px 20px 0 50px; font-size: 18px; } \
+// 		#pages a { text-decoration: none; } \
+// 		#pages li { margin: 5px 0; } \
+// 	</style> \
+// 	<ol id="pages"> \
+// 		<li><a href="about.html">About</a></li> \
+// 		<li><a href="index.html">Index</a></li> \
+// 	</ol> \
+// </div>');
